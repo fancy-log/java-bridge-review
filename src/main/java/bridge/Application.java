d4e820f4e647945eae29208f0cd8b1359195e2f5
junyong <jymoon1121@gmail.com>
2022-12-02 20:06:48 +0900

    fix(Application): main에 try-catch문 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -13,9 +13,14 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        init();
-        gameStart();
-        printGameResult();
+        try {
+            init();
+            gameStart();
+            printGameResult();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     private static void init() {
